299d2196f5c919a5798e361963b1237cb70f6dc7
Timur-Ziganshin <timur_zig99@mail.ru>
2017-07-28 19:20:59 +0300

    Чтобы было возможно сохранять

--- canvas_action.js
+++ canvas_action.js
@@ -1,11 +1,16 @@
 "use strict";
 
 
+//var c = document.getElementById( "topCanvas" );
+//var ctx = c.getContext( "2d" );
+
+//var bottomCanvas = document.getElementById( "bottomCanvas" );
+//var bottom_ctx = bottomCanvas.getContext( "2d" );
 var c = document.getElementById( "topCanvas" );
-var ctx = c.getContext( "2d" );
+var bottom_ctx = c.getContext( "2d" );
 
 var bottomCanvas = document.getElementById( "bottomCanvas" );
-var bottom_ctx = bottomCanvas.getContext( "2d" );
+var ctx = bottomCanvas.getContext( "2d" );
 
 /// Изначально пытался запихнать Line в отделтный файл objects.js,
 /// но не получилось импортировать класс от туда.
@@ -20,6 +25,7 @@ class Line {
     }
 
     drawTop() {
+        console.log('drawtop');
         ctx.beginPath();
         ctx.moveTo( this.pos1x, this.pos1y );
         ctx.lineTo( this.pos2x, this.pos2y );
@@ -28,6 +34,7 @@ class Line {
     }
 
     drawBottom() {
+        console.log('drawbot');
         bottom_ctx.beginPath();
         bottom_ctx.moveTo( this.pos1x, this.pos1y );
         bottom_ctx.lineTo( this.pos2x, this.pos2y );
@@ -72,12 +79,14 @@ c.onmouseleave = function( event ) { endDrawing( event ) };
 
 /// Когда появятся другие элементы(круг и тд) должно быть изменено
 function startDrawing( event ) {
+    console.log('start');
     curObject = new Line( pos.x, pos.y, pos.x, pos.y );
     curDrawing = setInterval( changeAndDraw, 1 );
 }
 
 
 function changeAndDraw() {
+    console.log('change delete');
     curObject.set2( pos.x, pos.y );
     ctx.clearRect( 0, 0, c.width, c.height );
     curObject.drawTop();
@@ -85,6 +94,7 @@ function changeAndDraw() {
 
 /// Переносит результат на bottomCanvas
 function endDrawing( event ) {
+    console.log('end');
     curObject.drawBottom();
     clearInterval( curDrawing );
 }
